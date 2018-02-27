61041fc181da084bac0e6af99dda1b23ba068b6c
吴建阳 <891873508@qq.com>
2018-02-27 20:26:39 +0800

    接口添加

--- baobozhineng/NewWorkManager/APIManager.h
+++ baobozhineng/NewWorkManager/APIManager.h
@@ -15,50 +15,90 @@
 @interface APIManager : NSObject
 ///单例对象
 + (APIManager *)sharedManager;
-//发送验证码
+#pragma mark 短信获取
 - (void)sendCodeWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//注册用户
+#pragma mark 用户注册
 - (void)registerWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//用户登录
+#pragma mark 用户登录
 - (void)loginWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//忘记密码
+#pragma mark 忘记密码
 - (void)forgetpwdWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//添加RF主机接口
+#pragma mark 添加RF主机接口
 - (void)deviceRegisterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//修改主机名称
+#pragma mark 修改主机名称
 - (void)deviceEditMasterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//获取主机IP
+#pragma mark 获取主机IP
 - (void)deviceGetIpWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//获取服务器时间
+#pragma mark 获取服务器时间
 - (void)deviceTimeWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//获取天气
+#pragma mark 获取天气
 - (void)deviceWeatherWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//获取房间
+#pragma mark 获取房间
 - (void)deviceGetMasterRoomWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//查询主机是否在线
+#pragma mark 查询主机是否在线
 - (void)deviceGetMasterStatusWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//onenet发送命令接口
+#pragma mark onenet发送命令接口
 - (void)deviceCmdsWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//单项设备添加
+#pragma mark 单项设备添加
 - (void)deviceAddOnewayDeviceWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//获取设备列表
+#pragma mark 获取设备列表
 - (void)deviceGetDeviceInfoWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//切换主机
+#pragma mark 切换主机
 - (void)deviceSwapMasterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//注册zigbee主机
+#pragma mark 注册zigbee主机
 - (void)deviceZigbeeRegisterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//绑定zigbee主机用户
+#pragma mark 绑定zigbee主机用户
 - (void)deviceBindZigbeeUserWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 添加双向开关设备
+- (void)deviceAddTwowaySwitchWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
 //添加zigbee设备
-- (void)deviceAddZigbeeDeviceWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//发送zigbee命令
+//- (void)deviceAddZigbeeDeviceWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 发送zigbee命令
 - (void)deviceZigbeeCmdsWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//获取分享主机二维码
+#pragma mark 获取分享主机二维码
 - (void)deviceGetShareCodeWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//分享主机
+#pragma mark 分享主机
 - (void)deviceShareMasterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//添加传感器接口
+#pragma mark 添加传感器接口
 - (void)deviceAddSensorWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
-//添加情景接口
+#pragma mark 添加情景接口
 - (void)deviceAddSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 修改情景
+- (void)deviceEditSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 删除情景
+- (void)deviceDeleteSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 获取设备的唯一devid
+- (void)deviceGetDevidWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 获取主机的设备列表
+- (void)deviceGetMasterDevicesWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 数值类设备数据上报
+- (void)deviceReportNumEquipmentWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 触发器（情景）信息上报
+- (void)deviceReportTriggerWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 手动执行触发器（情景）接口
+- (void)deviceTriggerSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 获取主机设备快捷键列表
+- (void)deviceGetDeviceShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 添加设备快捷键
+- (void)deviceAddDeviceShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 添加设备快捷键
+- (void)deviceDeleteDeviceShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 获取情景快捷键
+- (void)deviceGetSceneShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 添加情景快捷键
+- (void)deviceAddSceneShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 删除情景快捷键
+- (void)deviceDeleteSceneShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 获取情景触发列表
+- (void)deviceGetTriggerListWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 获取数值类上报的记录（传感器上报记录）
+- (void)deviceEquipmentReportLogWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 修改双向开关
+- (void)deviceEditTwowaySwitchWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 删除双向开关
+- (void)deviceDeleteTwowaySwitchWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 修改传感器
+- (void)deviceEditSensorWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
+#pragma mark 删除传感器
+- (void)deviceDeleteSensorWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure;
 @end
--- baobozhineng/NewWorkManager/APIManager.m
+++ baobozhineng/NewWorkManager/APIManager.m
@@ -26,124 +26,224 @@
     NSString *path = [NSString stringWithFormat:@"%@%@",API_HOST,str];
     return path;
 }
-//发送验证码
+#pragma mark 短信获取
 - (void)sendCodeWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     
     NSString *path = [self getPathWithInterface:@"/user/index/sendCode"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
     //    [AFNHttpTool getRequestWithUrl:path params:dic success:success failure:failure];
 }
-//注册用户
+#pragma mark 用户注册
 - (void)registerWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     
     NSString *path = [self getPathWithInterface:@"/user/index/register"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//用户登录
+#pragma mark 用户登录
 - (void)loginWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     
     NSString *path = [self getPathWithInterface:@"/user/index/login"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//忘记密码
+#pragma mark 忘记密码
 - (void)forgetpwdWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     
     NSString *path = [self getPathWithInterface:@"/user/index/forgetpwd"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//添加RF主机接口
+#pragma mark 添加RF主机接口
 - (void)deviceRegisterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/register"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//修改主机名称
+#pragma mark 修改主机名称
 - (void)deviceEditMasterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/edit_master"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//获取主机IP
+#pragma mark 获取主机IP
 - (void)deviceGetIpWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/get_ip"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//获取服务器时间
+#pragma mark 获取服务器时间
 - (void)deviceTimeWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/time"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//获取天气
+#pragma mark 获取天气
 - (void)deviceWeatherWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/weather"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//获取房间
+#pragma mark 获取房间
 - (void)deviceGetMasterRoomWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/get_master_room"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//查询主机是否在线
+#pragma mark 查询主机是否在线
 - (void)deviceGetMasterStatusWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/get_master_status"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//onenet发送命令接口
+#pragma mark onenet发送命令接口
 - (void)deviceCmdsWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/cmds"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//单项设备添加
+#pragma mark 单项设备添加
 - (void)deviceAddOnewayDeviceWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/device/add_oneway_device"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//获取设备列表
+#pragma mark 获取设备列表
 - (void)deviceGetDeviceInfoWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/get_device_info"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//切换主机
+#pragma mark 切换主机
 - (void)deviceSwapMasterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/swap_master"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//注册zigbee主机
+#pragma mark 注册zigbee主机
 - (void)deviceZigbeeRegisterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/zigbee_register"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//绑定zigbee主机用户
+#pragma mark 绑定zigbee主机用户
 - (void)deviceBindZigbeeUserWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/bind_zigbee_user"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//添加zigbee设备
-- (void)deviceAddZigbeeDeviceWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
-    NSString *path = [self getPathWithInterface:@"/device/device/add_zigbee_device"];
+#pragma mark 添加双向开关设备
+- (void)deviceAddTwowaySwitchWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/add_twoway_switch"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//发送zigbee命令
+#pragma mark 添加zigbee设备
+//- (void)deviceAddZigbeeDeviceWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+//    NSString *path = [self getPathWithInterface:@"/device/device/add_zigbee_device"];
+//    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+//}
+#pragma mark 发送zigbee命令
 - (void)deviceZigbeeCmdsWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/zigbee_cmds"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//获取分享主机二维码
+#pragma mark 获取分享主机二维码
 - (void)deviceGetShareCodeWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/get_share_code"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//分享主机
+#pragma mark 分享主机
 - (void)deviceShareMasterWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/share_master"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//添加传感器接口
+#pragma mark 添加传感器接口
 - (void)deviceAddSensorWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/index/add_sensor"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
-//添加情景接口
+#pragma mark 添加情景接口
 - (void)deviceAddSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
     NSString *path = [self getPathWithInterface:@"/device/scene/add_scene"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 修改情景
+- (void)deviceEditSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/scene/edit_scene"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 删除情景
+- (void)deviceDeleteSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/scene/delete_scene"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 获取设备的唯一devid
+- (void)deviceGetDevidWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/get_devid"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 获取主机的设备列表
+- (void)deviceGetMasterDevicesWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/get_master_devices"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 数值类设备数据上报
+- (void)deviceReportNumEquipmentWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/report_num_equipment"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 触发器（情景）信息上报
+- (void)deviceReportTriggerWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/scene/report_trigger"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 手动执行触发器（情景）接口
+- (void)deviceTriggerSceneWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/scene/trigger_scene"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 获取主机设备快捷键列表
+- (void)deviceGetDeviceShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/shortcut/get_device_shortcut"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 添加设备快捷键
+- (void)deviceAddDeviceShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/shortcut/add_device_shortcut"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 添加设备快捷键
+- (void)deviceDeleteDeviceShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/shortcut/delete_device_shortcut"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 获取情景快捷键
+- (void)deviceGetSceneShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/shortcut/get_scene_shortcut"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 添加情景快捷键
+- (void)deviceAddSceneShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/shortcut/add_scene_shortcut"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 删除情景快捷键
+- (void)deviceDeleteSceneShortcutWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/shortcut/delete_scene_shortcut"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 获取情景触发列表
+- (void)deviceGetTriggerListWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/scene/get_trigger_list"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 获取数值类上报的记录（传感器上报记录）
+- (void)deviceEquipmentReportLogWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/equipment_report_log"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 修改双向开关
+- (void)deviceEditTwowaySwitchWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/edit_twoway_switch"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 删除双向开关
+- (void)deviceDeleteTwowaySwitchWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/delete_twoway_switch"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 修改传感器
+- (void)deviceEditSensorWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/edit_sensor"];
+    [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
+}
+#pragma mark 删除传感器
+- (void)deviceDeleteSensorWithParameters:(NSDictionary *)dic success:(void(^)(id data))success failure:(void(^)(NSError *error))failure {
+    NSString *path = [self getPathWithInterface:@"/device/device/delete_sensor"];
     [AFNHttpTool postRequestWithUrl:path params:dic success:success failure:failure];
 }
 @end
